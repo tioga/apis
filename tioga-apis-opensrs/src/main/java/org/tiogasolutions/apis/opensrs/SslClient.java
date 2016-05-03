bb5492c30821b9d224c05e7c2cebae9f65c65044
Jacob D Parr <me@jacobparr.com>
2016-05-03 12:09:51 -0700

    Refactoring tests

--- tioga-apis-opensrs/src/main/java/org/tiogasolutions/apis/opensrs/SslClient.java
+++ tioga-apis-opensrs/src/main/java/org/tiogasolutions/apis/opensrs/SslClient.java
@@ -85,12 +85,12 @@ public class SslClient {
     this.privateKey = privateKey;
   }
 
-  protected String md5Sum(String str) throws NoSuchAlgorithmException {
+  protected static String md5Sum(String str) throws NoSuchAlgorithmException {
     MessageDigest md5 = MessageDigest.getInstance("MD5");
     return String.format("%032x", new BigInteger(1, md5.digest(str.getBytes())));
   }
 
-  public String getSignature(String xml) throws NoSuchAlgorithmException {
+  public static String getSignature(String xml, String privateKey) throws NoSuchAlgorithmException {
     return md5Sum(md5Sum(xml + privateKey) + privateKey);
   }
 
@@ -104,7 +104,7 @@ public class SslClient {
 
     String portStr = String.valueOf(port);
     Protocol.registerProtocol("https", new Protocol("https", (ProtocolSocketFactory)new MySSLSocketFactory(), port));
-    String signature = getSignature(xml);
+    String signature = getSignature(xml, privateKey);
     String uri = "https://" + host + ":" + portStr + "/";
     PostMethod postRequest = new PostMethod(uri);
     postRequest.addRequestHeader("Content-Length", String.valueOf(xml.length()));
@@ -124,50 +124,5 @@ public class SslClient {
     }
 
     return postRequest.getResponseBodyAsString();
-  }
-
-  public static void main(String[] args) {
-
-    String privateKey = EnvUtils.requireProperty(OpenSrsApi.OPEN_SRS_API_KEY);
-    String userName = EnvUtils.requireProperty(OpenSrsApi.OPEN_SRS_API_LOGIN);
-
-    int port = OpenSrsPort.secure.getPort();
-    String host = OpenSrsEnv.live.getHost();
-
-    String xml = "<?xml version='1.0' encoding='UTF-8' standalone='no' ?>" +
-        "<?xml version='1.0' encoding='UTF-8' standalone='no' ?>"+
-        "<!DOCTYPE OPS_envelope SYSTEM 'ops.dtd'>"+
-        "<OPS_envelope>"+
-          "<header>"+
-            "<version>0.9</version>"+
-            "<msg_id>2.21765911726198</msg_id>"+
-            "<msg_type>standard</msg_type>"+
-          "</header>"+
-          "<body>"+
-            "<data_block>"+
-              "<dt_assoc>"+
-                "<item key='attributes'>"+
-                  "<dt_assoc>"+
-                    "<item key='domain'>shamingstick.com</item>"+
-                    "<item key='pre-reg'>0</item>"+
-                  "</dt_assoc>"+
-                "</item>"+
-                "<item key='object'>DOMAIN</item>"+
-                "<item key='action'>LOOKUP</item>"+
-                "<item key='protocol'>XCP</item>"+
-              "</dt_assoc>"+
-            "</data_block>"+
-          "</body>"+
-        "</OPS_envelope>";
-
-    SslClient sslclient = new SslClient(host, port, userName, privateKey);
-
-    try {
-      String response = sslclient.sendRequest(xml);
-      System.out.println("\nResponse is:\n" + response);
-
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 }
