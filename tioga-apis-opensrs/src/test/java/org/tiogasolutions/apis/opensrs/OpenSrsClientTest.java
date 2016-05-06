e19558699585e6043eefe9c11827069cc34e542d
Jacob D Parr <me@jacobparr.com>
2016-05-05 19:02:27 -0700

    Fixed broken unit test

--- tioga-apis-opensrs/src/test/java/org/tiogasolutions/apis/opensrs/OpenSrsClientTest.java
+++ tioga-apis-opensrs/src/test/java/org/tiogasolutions/apis/opensrs/OpenSrsClientTest.java
@@ -1,12 +1,23 @@
 package org.tiogasolutions.apis.opensrs;
 
 import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.tiogasolutions.apis.opensrs.pub.DnLookupResponse;
 import org.tiogasolutions.apis.opensrs.pub.DnStatus;
+import org.tiogasolutions.dev.common.EnvUtils;
 
 @Test
 public class OpenSrsClientTest {
+
+    @BeforeClass
+    public void beforeClass() {
+        String apiLogin = EnvUtils.findProperty("OPEN_SRS_API_LOGIN");
+        if (apiLogin == null) {
+        throw new SkipException("Open SRS API authentication was not found.");
+        }
+    }
 
     public void testAvailableDomain_HC() throws Exception {
         OpenSrsClient client = OpenSrsClient.liveHttps(new HttpClientDelegate(10));
