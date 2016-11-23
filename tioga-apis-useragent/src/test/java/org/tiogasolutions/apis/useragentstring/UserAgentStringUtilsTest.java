2fe565a160dac801072cb2259e26cd4ca87063a0
Jacob D Parr <me@jacobparr.com>
2016-11-22 19:46:32 -0800

    Dependency updates

--- tioga-apis-useragent/src/test/java/org/tiogasolutions/apis/useragentstring/UserAgentStringUtilsTest.java
+++ tioga-apis-useragent/src/test/java/org/tiogasolutions/apis/useragentstring/UserAgentStringUtilsTest.java
@@ -3,10 +3,10 @@ package org.tiogasolutions.apis.useragentstring;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
+import java.net.ConnectException;
 import java.net.UnknownHostException;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class UserAgentStringUtilsTest {
 
@@ -16,7 +16,7 @@ public class UserAgentStringUtilsTest {
     try {
       String uas = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36";
       uam = UserAgentStringUtils.lookupUserAgent(uas);
-    } catch (UnknownHostException e) {
+    } catch (UnknownHostException | ConnectException e) {
       throw new SkipException("Offline", e);
     }
 
